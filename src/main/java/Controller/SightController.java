d7d6fa591c66b5d52c5b47051b7f53aea9aa0e4c
zearthur99 <ze_arthur99@hotmail.com>
2018-09-23 21:41:17 -0300

    Controller adicionado

---
+++ src/main/java/Controller/SightController.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Dao.Dao;
+
+
+/**
+ *
+ * @author zearthur99
+ */
+public class SightController {
+    private Dao d = Dao.getInstance();
+}
